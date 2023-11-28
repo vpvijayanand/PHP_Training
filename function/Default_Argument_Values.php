0d7d3b76a059b453ef0f86cc8551ed5dfae2380f
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-28 08:47:33 +0530

    Create Default_Argument_Values.php

---
+++ function/Default_Argument_Values.php
@@ -0,0 +1,8 @@
+<?php
+function setHeight($minheight = 50) {
+    echo "The height is : $minheight <br>";
+}
+
+setHeight(350); // Outputs: The height is 350
+setHeight();    // Outputs: The height is 50
+?>
