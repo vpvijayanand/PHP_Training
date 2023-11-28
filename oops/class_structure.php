0b082921ed2c74fcde644f89706a98833fbc8320
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-28 08:53:25 +0530

    Create class_structure.php

---
+++ oops/class_structure.php
@@ -0,0 +1,12 @@
+<?php
+class Car {
+    // properties and methods go here
+    public $color;
+    // Methods
+   function set_color($color) {
+        $this->color = $color;
+    }
+}
+$myCar = new Car();
+
+?>
