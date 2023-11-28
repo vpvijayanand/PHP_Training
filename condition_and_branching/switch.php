2e852aa0df68a2999eaed2cc85d0e203c10720f8
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-28 08:35:22 +0530

    Create switch.php

---
+++ condition_and_branching/switch.php
@@ -0,0 +1,16 @@
+<?php
+$day = "Tue";
+switch ($day) {
+    case "Mon":
+        echo "Today is Monday.";
+        break;
+    case "Tue":
+        echo "Today is Tuesday.";
+        break;
+    case "Wed":
+        echo "Today is Wednesday.";
+        break;
+    default:
+        echo "No information available for this day.";
+}
+?>
