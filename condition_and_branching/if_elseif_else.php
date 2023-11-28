711c5be643b7abec79635dba4f65e882c421c171
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-28 08:34:38 +0530

    Create if_elseif_else.php

---
+++ condition_and_branching/if_elseif_else.php
@@ -0,0 +1,10 @@
+<?php
+$age = 18;
+if ($age > 18) {
+    echo "You are eligible to vote.";
+} elseif ($age == 18) {
+    echo "You are just eligible to vote.";
+} else {
+    echo "You are not eligible to vote.";
+}
+?>
